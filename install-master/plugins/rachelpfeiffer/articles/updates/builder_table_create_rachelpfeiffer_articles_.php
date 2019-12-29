15615855d1430b14fa143835e1e6a17d2608ce54
Rachel Pfeiffer <rachelpfeifferdevelopment@gmail.com>
2019-12-29 22:07:35 +0200

    add article plugin

---
+++ install-master/plugins/rachelpfeiffer/articles/updates/builder_table_create_rachelpfeiffer_articles_.php
@@ -0,0 +1,25 @@
+<?php namespace RachelPfeiffer\Articles\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateRachelpfeifferArticles extends Migration
+{
+    public function up()
+    {
+        Schema::create('rachelpfeiffer_articles_', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->text('title');
+            $table->text('author');
+            $table->date('date');
+            $table->text('category');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('rachelpfeiffer_articles_');
+    }
+}
---
+++ install-master/plugins/rachelpfeiffer/articles/updates/builder_table_update_rachelpfeiffer_articles_.php
@@ -0,0 +1,23 @@
+<?php namespace RachelPfeiffer\Articles\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateRachelpfeifferArticles extends Migration
+{
+    public function up()
+    {
+        Schema::table('rachelpfeiffer_articles_', function($table)
+        {
+            $table->text('content');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('rachelpfeiffer_articles_', function($table)
+        {
+            $table->dropColumn('content');
+        });
+    }
+}
